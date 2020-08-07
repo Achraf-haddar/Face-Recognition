46c5f72543681268b034fbc04432419bddfe18b4
Achraf-haddar <achraf.haddar@supcom.tn>
2020-08-07 11:07:29 +0200

    labels from directories + training image into a numpy array

--- src/faces-train.py
+++ src/faces-train.py
@@ -1,12 +1,27 @@
 import os
+import numpy as np
+from PIL import Image
 # Path of Base directory
 BASE_DIR = os.path.dirname(os.path.abspath(__file__ ))
 # Path of images directory (join Base_dir + "images")
 image_dir = os.path.join(BASE_DIR, "images")
+
+x_train = []
+y_labels = []
+
+
+
 # Python method walk() generates the file names in a directory
 for root, dirs, files in os.walk(image_dir):
     for file in files: # files is a list of file's path
     # check if the file is a png or jpg file
         if file.endswith("png") or file.endswith("jpg"): 
             path = os.path.join(root, file) # get the path of the image
-            print(path)
+            # get the label from directory name
+            label = os.path.basename(root).replace(" ", "-").lower()
+            print(label)
+            # Read the image using pillow
+            pil_image = Image.open(path).convert("L") # Grayscale
+            # Store the image into array
+            image_array = np.array(pil_image, 'uint8')
+            print(image_array)
